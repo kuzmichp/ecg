b411ad262bf4ddc8b2d34a28e90d925c44717efb
kuzmichp <kuzmichpa@gmail.com>
2015-04-30 11:25:06 +0200

    WPF application with ManApps library has been created.

---
+++ project/ECGAnalysisSystem/ECGAnalysisSystem/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using PropertyChanged;
+
+namespace ECGAnalysisSystem.ViewModel
+{
+    /// <summary>
+    /// ViewModel class for main application view
+    /// </summary>
+    [ImplementPropertyChanged]
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        public ICommand Load
+        {
+            get { return new RelayCommand<object>(LoadExecute, () => true); }
+        }
+
+        private async void LoadExecute(object o)
+        {
+            MetroWindow metroWindow = o as MetroWindow;
+            await metroWindow.ShowMessageAsync("This is the title", "Some message");
+        }
+    }
+}
---
+++ project/ECGAnalysisSystem/ECGAnalysisSystem/ViewModel/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ECGAnalysisSystem.ViewModel
+{
+    public class RelayCommand<T> : ICommand where T : class
+    {
+        private Action<T> methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+
+        public RelayCommand(Action<T> methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        public RelayCommand(Action<T> methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = this.canExecuteEvaluator.Invoke();
+                return result;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.methodToExecute.Invoke(parameter as T);
+        }
+    }
+}
